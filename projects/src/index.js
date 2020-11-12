839cfce62ce2dc96df126770cbc85d6028ab3f62
Galelya <s.galina97@mail.ru>
2020-11-12 15:59:34 +0300

    returnFirstArgument

--- projects/src/index.js
+++ projects/src/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(arg) {
+  return arg;
+}
 
 /*
  Задание 2:
